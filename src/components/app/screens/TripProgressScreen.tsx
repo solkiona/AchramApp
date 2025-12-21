a5a4453abd24b15e4ee4534c0e368a6613a646dd
SOLKIONA <solkiona@gmail.com>
2025-12-21 16:42:09 +0100

    fixed the dissappearing driver icon marker by making it a stable function reference

--- src/components/app/screens/TripProgressScreen.tsx
+++ src/components/app/screens/TripProgressScreen.tsx
@@ -40,25 +40,10 @@ const mapStyles = [
   }
 ];
 
-const getDriverIcon = () => ({
-  path: "M18.92 6.01C18.72 5.42 18.16 5 17.5 5h-11c-.66 0-1.22.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.5 16c-.83 0-1.5-.67-1.5-1.5S5.67 13 6.5 13s1.5.67 1.5 1.5S7.33 16 6.5 16zm11 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM5 11l1.5-4.5h11L19 11H5z",
-  fillColor: "#F59E0B",
-  fillOpacity: 1,
-  strokeColor: "#FFFFFF",
-  strokeWeight: 2.5,
-  scale: 2.2,
-  anchor: { x: 12, y: 12 } as google.maps.Point,
-});
 
-const getDestinationIcon = () => ({
-  path: "M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z",
-  fillColor: "#EF4444",
-  fillOpacity: 1,
-  strokeColor: "#FFFFFF",
-  strokeWeight: 2.5,
-  scale: 2.2,
-  anchor: { x: 12, y: 24 } as google.maps.Point,
-});
+
+
+
 
 export default function TripProgressScreen({
   driver,
@@ -94,6 +79,33 @@ export default function TripProgressScreen({
       lng: coord[0],
     }));
   }, [airportPickupArea]);
+
+
+
+const getDestinationIcon = useCallback(
+  () => ({
+  path: "M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z",
+  fillColor: "#EF4444",
+  fillOpacity: 1,
+  strokeColor: "#FFFFFF",
+  strokeWeight: 2.5,
+  scale: 2.2,
+  anchor: { x: 12, y: 24 } as google.maps.Point,
+})  
+  ,[])
+
+  const getDriverIcon = useCallback(
+  () => ({
+  path: "M18.92 6.01C18.72 5.42 18.16 5 17.5 5h-11c-.66 0-1.22.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.5 16c-.83 0-1.5-.67-1.5-1.5S5.67 13 6.5 13s1.5.67 1.5 1.5S7.33 16 6.5 16zm11 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM5 11l1.5-4.5h11L19 11H5z",
+  fillColor: "#F59E0B",
+  fillOpacity: 1,
+  strokeColor: "#FFFFFF",
+  strokeWeight: 2.5,
+  scale: 2.2,
+  anchor: { x: 12, y: 12 } as google.maps.Point,
+})
+  , []
+)
 
   const polygonPaths = getPolygonPaths();
 
