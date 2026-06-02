01aba0c90e5fd6838b1314e465e6e8757060f1f7
SOLKIONA <solkiona@gmail.com>
2026-06-02 14:11:54 +0100

    Diagnosing biometric 5

--- src/providers/ClientProviders.tsx
+++ src/providers/ClientProviders.tsx
@@ -3,7 +3,7 @@
 
 import { AuthProvider } from '@/contexts/AuthContext';
 import QueryProvider from './QueryProvider'; // Assume this exists
-import { BiometricGate } from '@/app/BiometricGate';
+
 
 interface ClientProvidersProps {
   children: React.ReactNode;
@@ -12,13 +12,9 @@ interface ClientProvidersProps {
 export default function ClientProviders({ children }: ClientProvidersProps) {
   return (
     <AuthProvider>
-      <BiometricGate>
-
       <QueryProvider>
         {children}
       </QueryProvider>
-
-      </BiometricGate>
     </AuthProvider>
   );
 }
