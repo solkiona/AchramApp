8ca21bc71f0cca64fbadcff720d4eaaa3fb48e8a
SOLKIONA <solkiona@gmail.com>
2026-06-06 19:43:29 +0100

    keyboard fix test

--- android/app/src/main/java/com/achrams/ride1/MainActivity.java
+++ android/app/src/main/java/com/achrams/ride1/MainActivity.java
@@ -1,5 +1,20 @@
 package com.achrams.ride1;
 
+import android.os.Bundle;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Re-apply adjustPan AFTER Capacitor sets up edge-to-edge
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+    }
+}
+
+// package com.achrams.ride1;
+
+// import com.getcapacitor.BridgeActivity;
+
+// public class MainActivity extends BridgeActivity {}
