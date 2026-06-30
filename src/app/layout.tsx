f5a793a8e8408fedc45aae5fec8abd7a84907078
SOLKIONA <solkiona@gmail.com>
2026-07-01 00:11:35 +0100

    Isolated biometrics only for android -rollback 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,11 +34,11 @@ export default function RootLayout({
         <main className="flex-1 min-h-0 flex flex-col">
 
         <ClientProviders>
-          <BiometricGate>
+          {/* <BiometricGate> */}
           <PostHogProvider>
           {children}
           </PostHogProvider>
-          </BiometricGate>
+          {/* </BiometricGate> */}
         </ClientProviders>
 
         </main>
